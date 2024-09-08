083f5549cf430e8a9280054ac8f23a0a7d7acb8c
Matin <matinhosseiniofficial@gmail.com>
2024-09-08 15:19:10 +0330

    added header and top bar html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
-
-import { useRoutes } from 'react-router-dom'
-import './App.css'
-import routes from './routes'
+import { useRoutes } from "react-router-dom";
+import "./App.css";
+import routes from "./routes";
+import Header from "./components/Header";
 
 function App() {
-
-  const router = useRoutes(routes)
-  
+  const router = useRoutes(routes);
 
   return (
     <>
-    {router}
+      <Header />
+      {router}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
