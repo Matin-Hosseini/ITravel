ae62333ee20319d765e05da16e83a043f5b8d95b
Matin <matinhosseiniofficial@gmail.com>
2024-09-09 14:59:15 +0330

    added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useRoutes } from "react-router-dom";
 import "./App.css";
 import routes from "./routes";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const router = useRoutes(routes);
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       {router}
+      <Footer />
     </>
   );
 }
